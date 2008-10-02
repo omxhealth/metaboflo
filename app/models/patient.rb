3b6765ed6ba7ba91a27074277305e10f79d9d9ae
craigknox <craigknox@22328df5-9857-0410-bb3d-c9ff41ad327f>
2008-10-02 23:28:18 +0000

    Sample now requires a patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,8 @@ class Patient < ActiveRecord::Base
   
   has_many :cohort_assignments, :dependent => :destroy
   has_many :cohorts, :through => :cohort_assignments
+  
+  def name
+    "#{self.first_name} #{self.middle_name} #{self.last_name}"
+  end
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,5 +1,7 @@
 class Sample < ActiveRecord::Base
   belongs_to :patient
   has_many :data_files
+  
+  validates_presence_of :patient_id
   # belongs_to :taken_by, :class_name => 'User', :foreign_key => 'taken_by_id'
 end
