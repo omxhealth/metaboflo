efa8673d9247fdfc00acd4cc36a9e0da67b1a3bf
eisner <eisner@22328df5-9857-0410-bb3d-c9ff41ad327f>
2008-10-28 23:49:12 +0000

    Added a boolean_to_english method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def boolean_to_english(val)
+    if val == true
+      return 'yes'
+    else
+      return 'no'
+    end
+  end
 end
