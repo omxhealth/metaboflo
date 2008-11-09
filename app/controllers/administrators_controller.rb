81e9094474603021ce22ce7989d9ccaa773c0d3a
joseph <joseph@22328df5-9857-0410-bb3d-c9ff41ad327f>
2008-11-09 02:28:33 +0000

    - added filter to allow only superusers and administrators to access admin page

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -2,4 +2,7 @@ class AdministratorsController < ApplicationController
   def index
   end
 
+  def authorized?
+    current_user.rank == 'Superuser' || current_user.rank == 'Administrator'
+  end
 end
--- test/functional/administrators_controller_test.rb
+++ test/functional/administrators_controller_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class AdministratorsControllerTest < ActionController::TestCase
   def test_should_get_index
+    [:superuser, :admin].each do |user|
+      login_as user
+      get :index
+      assert_response :success
+    end
+  end
+  
+  def test_should_not_get_index_not_permitted
     login_as :user
     get :index
-    assert_response :success
+    assert_response :redirect
   end
 end
