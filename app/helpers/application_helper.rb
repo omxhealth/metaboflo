9a3019bf8585c19d424361fb4cabdf38936297f7
eisner <eisner@22328df5-9857-0410-bb3d-c9ff41ad327f>
2008-12-13 21:31:17 +0000

    Moved edit, show, delete, new links to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,22 @@ module ApplicationHelper
     else
       return 'no'
     end
-  end    
+  end
+  
+  def destroy_link(params)
+    return link_to('Delete', params, {:confirm => 'Are you sure?', :method => :delete, :class => 'icon icon-del'} )
+  end
+  
+  def edit_link(params)
+    return link_to('Edit', params, :class => 'icon icon-edit' )
+  end
+  
+  def show_link(params)
+    return link_to('Show', params, :class => 'icon icon-show' )
+  end
+  
+  def new_link(name, params)
+    return link_to("New #{name}", params, :class => 'icon icon-add' )
+  end
+  
 end
