9b24a11d3c1ac1266df1cf892ad6f44f0e1b625f
eisner <eisner@22328df5-9857-0410-bb3d-c9ff41ad327f>
2009-05-06 02:28:26 +0000

    Fixed animal attributes

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -11,19 +11,9 @@ class Animal < ActiveRecord::Base
   has_many :lab_tests, :order => 'collected_at ASC', :dependent => :destroy
   
   validates_presence_of :code, :site_id
-  validates_inclusion_of :gender, :in => ['Male', 'Female'], :allow_blank => true, :message => 'must be either Male or Female'
-  
-  validates_length_of :first_initial, :allow_blank => true, :maximum => 1, :message => "should be at most 1 character"
-  validates_length_of :middle_initial, :allow_blank => true, :maximum => 1, :message => "should be at most 1 character"
-  validates_length_of :last_initial, :allow_blank => true, :maximum => 1, :message => "should be at most 1 character"
   
   def name
-    "#{self.first_initial} #{self.middle_initial} #{self.last_initial}".gsub(/\s+/, " ").strip
-  end
-  
-  # Required so that Experiments, Samples, and Animals can be displayed in cohorts
-  def to_s
-    return "#{self.code} (initials: #{self.name})"
+    "Cow #{code}"
   end
   
 end
--- test/unit/animal_test.rb
+++ test/unit/animal_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class AnimalTest < ActiveSupport::TestCase
-  def test_name
-    assert_equal 'J F S', animals(:one).name
-    assert_equal '', Animal.new(:first_initial => '', :last_initial => '').name
-    assert_equal 'J B', Animal.new(:first_initial => 'J', :last_initial => 'B').name
+  # Replace this with your real tests.
+  def test_truth
+    assert true
   end
 end
