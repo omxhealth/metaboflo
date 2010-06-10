20331a58610229c0e83de5586eb455fda1df7680
knox <knox@22328df5-9857-0410-bb3d-c9ff41ad327f>
2010-06-10 03:19:14 +0000

    Capified.

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+# This application is configured to use 3 different capistrano environments. You need to install the capistrano-ext gem
+# to deploy to these environments:
+#
+# > gem install capistrano-ext
+#
+# You can then deploy to the different environments like so:
+#
+# > cap production deploy
+
+set :stages, %w(demo testing production)
+require 'capistrano/ext/multistage'
+
