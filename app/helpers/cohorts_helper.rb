4b2f7443cd3d2a5990458825464a29c8188744ce
Craig Knox <cknox@insiliflo.com>
2011-05-01 15:16:34 -0600

    Fixed test subject cohorts to use the test subject title.

--- app/helpers/cohorts_helper.rb
+++ app/helpers/cohorts_helper.rb
@@ -1,6 +1,9 @@
 module CohortsHelper
 
   def humanized_type(type)
-    type.to_s.tableize.singularize.gsub(/_/, " ").humanize
+    case type.to_s
+      when 'TestSubject' then TestSubject.title
+      else type.to_s.tableize.singularize.gsub(/_/, " ").humanize
+    end
   end
 end
