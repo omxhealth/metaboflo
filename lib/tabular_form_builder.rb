e55d29404ff90732a65f16d57832942bcf61f6d9
Joseph Cruz <joseph@jcruz.local>
2011-05-12 00:15:06 -0600

    Fixed labelled_tabular_form so that html is not escaped.

--- lib/tabular_form_builder.rb
+++ lib/tabular_form_builder.rb
@@ -25,15 +25,15 @@ class TabularFormBuilder < ActionView::Helpers::FormBuilder
   (field_helpers - %w(radio_button hidden_field label) + %w(date_select datetime_select)).each do |selector|
     src = <<-END_SRC
     def #{selector}(field, options = {}) 
-      return super if options.delete :no_label
+      return super.html_safe if options.delete :no_label
       label_text = options[:label] if options[:label]
       label_text ||= field.to_s.gsub(/\_id$/, "").humanize
       label_text << @template.content_tag("span", " *", :class => "required") if options.delete(:required)
-      label = self.label(field, label_text)
+      label = self.label(field, label_text.html_safe)
       # label = @template.content_tag("label", label_text, 
       #               :class => (@object && @object.errors[field] ? "error" : nil), 
       #               :for => (@object_name.to_s + "_" + field.to_s))
-      label + super
+      (label + super).html_safe
     end
     END_SRC
     class_eval src, __FILE__, __LINE__
@@ -44,25 +44,25 @@ class TabularFormBuilder < ActionView::Helpers::FormBuilder
   end
   
   def select(field, choices, options = {}, html_options = {}) 
-    return super if options.delete :no_label
+    return super.html_safe if options.delete :no_label
     label_text = options[:label] if options[:label]
     label_text ||= field.to_s.gsub(/\_id$/, "").humanize + (options.delete(:required) ? @template.content_tag("span", " *", :class => "required"): "")
-    label = self.label(field, label_text)
+    label = self.label(field, label_text.html_safe)
     # label = @template.content_tag("label", label_text, 
     #               :class => (@object && @object.errors[field] ? "error" : nil), 
     #               :for => (@object_name.to_s + "_" + field.to_s))
-    label + super
+    (label + super).html_safe
   end
 
   def country_select(field, priority_countries = nil, options = {}, html_options = {}) 
-    return super if options.delete :no_label
+    return super.html_safe if options.delete :no_label
     label_text = options[:label] if options[:label]
     label_text ||= field.to_s.gsub(/\_id$/, "").humanize + (options.delete(:required) ? @template.content_tag("span", " *", :class => "required"): "")
-    label = self.label(field, label_text)
+    label = self.label(field, label_text.html_safe)
     # label = @template.content_tag("label", label_text, 
     #               :class => (@object && @object.errors[field] ? "error" : nil), 
     #               :for => (@object_name.to_s + "_" + field.to_s))
-    label + super
+    (label + super).html_safe
   end
   
   def check_box_group(field, choices, options= {}, html_option = {})
