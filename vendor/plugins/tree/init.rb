79cf0f9bc5f847c43eb2d51b868898ce08e4d753
Craig Knox <cknox@insiliflo.com>
2011-05-12 21:42:07 -0600

    Removed deprecated loading of tree.js. Removed lib/tree files.

--- vendor/plugins/tree/init.rb
+++
@@ -1,4 +0,0 @@
-ActionView::Helpers::AssetTagHelper::register_javascript_include_default "tree"
-# # Metaboflo::Application.config.action_view.javascript_expansions[:defaults] ||= []
-# # Metaboflo::Application.config.action_view.javascript_expansions[:defaults] << 'tree'
-# require 'lib/tree/railtie'
