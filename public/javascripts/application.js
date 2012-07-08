4f01880f0ed2647b22cd39241fc49b78e91c12e9
Craig Knox <knox@Craigs-MacBook-Air.local>
2012-07-08 15:38:41 -0700

    Removed unused assets. Moved plugins to lib.

--- public/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-function set_flash(flash_type, message) {
-	$("#flash").html("<p class=\"" + flash_type + "\">" + message + "</p>");
-}
