df9a28df596f352f877929eaea6179419b4b5efb
Tim Jewison <tjewison@ualberta.ca>
2012-08-09 22:34:37 -0600

    Added more SMPDB links

--- public/javascripts/sample_manifest.js
+++ public/javascripts/sample_manifest.js
@@ -19,42 +19,42 @@ $(function() {
 	});
 	$( "#module_3_dialog" ).dialog({
 		title: "Module 3 - Amino Acids, Urea cycle and Metabolites - LC/MS",
-		height: 350,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
 	});
 	$( "#module_4_dialog" ).dialog({
 		title: "Module 4 - Acyl Carnitines - LC/MS",
-		height: 150,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
 	});
 	$( "#module_5_dialog" ).dialog({
 		title: "Module 5 - Glycolysis, Krebs and the Pentose Cycle - GC/MS",
-		height: 200,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
 	});
 	$( "#gc_fap_dialog" ).dialog({
 		title: "GC-FAP - GC-FID Fatty acid profiling",
-		height: 200,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
 	});
 	$( "#ss_1_dialog" ).dialog({
 		title: "SS 1 - Lipogenesis by Deuterated H<sub>2</sub>O",
-		height: 200,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
 	});
 	$( "#ss_2_dialog" ).dialog({
 		title: "SS 2 - Stable Isotope intraperitoneal glucose tolerance test",
-		height: 200,
+		height: 600,
 		width: 1050,
 		modal: true,
 		autoOpen: false
@@ -66,7 +66,7 @@ $(function() {
 	$("#module_3").button().click(function(){$( "#module_3_dialog" ).dialog("open");});
 	$("#module_4").button().click(function(){$( "#module_4_dialog" ).dialog("open");});
 	$("#module_5").button().click(function(){$( "#module_5_dialog" ).dialog("open");});
-	$("#gc_fap").button().click(function(){$( "#gc_fag_dialog" ).dialog("open");});
+	$("#gc_fap").button().click(function(){$( "#gc_fap_dialog" ).dialog("open");});
 	$("#ss_1").button().click(function(){$( "#ss_1_dialog" ).dialog("open");});
 	$("#ss_2").button().click(function(){$( "#ss_2_dialog" ).dialog("open");});
 	$(".link_to").button().click(function(evt){window.open($(evt.currentTarget).data("url"));});
