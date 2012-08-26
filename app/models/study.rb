9ddc00f85f078dd40469ce531a8b23c5bf60d0df
Craig Knox <knox@new-host-3.home>
2012-08-25 21:45:47 -0700

    Fixed retarded study workflow bug probably written at 4 in the morning on the plane to Australia.

--- app/models/study.rb
+++ app/models/study.rb
@@ -2,5 +2,5 @@ class Study < ActiveRecord::Base
   has_many :cohorts
   validates_presence_of :name
   
-  accepts_nested_attributes_for :cohorts, :allow_destroy => true, :reject_if => :all_blank
+  accepts_nested_attributes_for :cohorts, :allow_destroy => true, :reject_if => proc { |a| a['label'].blank? }
 end
