8bae089d1ce7e1d94eea5a9a8145542d2f7f6d32
Craig Knox <knox@new-host-3.home>
2012-08-26 23:42:16 -0700

    Improved login structure.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,20 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  layout 'lims'
-
+  layout :layout_by_resource
+  protect_from_forgery
+  
   helper :all # include all helpers, all the time
   
   before_filter :authenticate_user!
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'a347d8932ace59d533b9767c870630f9'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  def layout_by_resource
+    if devise_controller? && resource_name == :client
+      'public'
+    else
+      'lims'
+    end
+  end
 
   # remove objects where root test_subject is from another site
   def find_for_site(objects, site)
